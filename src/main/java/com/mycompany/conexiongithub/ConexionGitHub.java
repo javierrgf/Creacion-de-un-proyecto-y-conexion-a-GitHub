dfcedce05eec39a6d05e4cdefbd1d4253c4492aa
javierrgf <javierrgf@gmail.comgit config --global user.email javierrgf@gmail.com>
2024-10-23 11:41:59 +0200

    Initial commit

---
+++ src/main/java/com/mycompany/conexiongithub/ConexionGitHub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.conexiongithub;
+
+/**
+ *
+ * @author Javi
+ */
+public class ConexionGitHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
